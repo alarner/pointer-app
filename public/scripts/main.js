3e226f87118bb15d25a9f68b99227ba7bc529e50
Aaron Larner <anlarner@gmail.com>
2016-04-21 00:12:02 -0500

    get error handling working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,28 @@
+import $ from 'jquery';
+import Backbone from 'backbone';
+import {sync, errorHandler} from './ajax';
+Backbone.sync = sync;
+$.ajaxSetup({
+	headers: {
+		Accept: 'application/json'
+	},
+	beforeSend: function(xhr) {
+		let error = this.error;
+		if(error) {
+			this.error = function(xhr) {
+				errorHandler(xhr, error);
+			};
+		}
+	}
+});
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from './components/App';
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
+
 
 const router = (
 	<Router history={browserHistory}>
@@ -15,5 +34,3 @@ const router = (
 );
 
 ReactDOM.render(router, document.getElementById('app'));
-
-console.log('test');
