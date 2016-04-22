f9c5ff90551a40702dd44159db1a6772a40ee79e
Maria Zamora <Zamora.Mariac@gmail.com>
2016-04-22 14:34:39 -0500

    added form

--- public/scripts/components/pages/Login.js
+++ public/scripts/components/pages/Login.js
@@ -4,14 +4,13 @@ export default React.createClass({
 	render: function() {
 		return (
 			<section className="page-login">
-				<div className="login-box"> Login
-					<input type="text" placeholder="email"/> 
-					<input type="text" placeholder="password"/>
+				<form className="login-box"> Login
+					<input type="text" placeholder="email"/>
+					<div className="error"> </div> 
+					<input type="password" placeholder="password"/>
+					<div className="error"> </div>
 					<button> Login </button>
-				</div>
-				
-
-
+				</form>
 			</section>
 		);
 	}
