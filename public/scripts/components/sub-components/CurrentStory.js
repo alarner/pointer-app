49c37b0ae1715b54a74746d90ccf0e7e8d6973fb
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-22 16:56:47 -0500

    started render function of CurrentStory.js

---
+++ public/scripts/components/sub-components/CurrentStory.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StoryCollection from './collections/StoryCollection';
+
+
+export default React.createClass({
+	render: function() {
+		return(
+			<section>
+				<h1>This is a Story Title</h1>
+				<div>
+					<img src=""/>
+					<p>This is a sample description of a story.</p>
+				</div>
+			</section>  
+			);
+	}
+
+});
+
+
+
