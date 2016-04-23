3bdd0208b9d164920d2e314fc498e964bde245ab
Katy <ihatetoast@gmail.com>
2016-04-23 14:21:48 -0500

    done until i have more info

--- public/scripts/components/pages/Stories.js
+++ public/scripts/components/pages/Stories.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import StoryCollection from './../collections/';
-import Backbone from 'backbone';
 import CategoriesPreview from './sub-components/Categories-preview.js';
 
-export default React.createClass({
-	render: function() {
-		return (
-			<section className="page-stories">
-				<h1>Stories</h1>
-			</section>
-		);
-	}
-});
 
-
-///fetch the list of stories
-// const StoryCollection = Backbone.Collection.extend({
-// 	model: Story,
-// 	url: '/api/v1/story'
-// });
-
-// export default new StoryCollection();
-
-// set the initial state
 const StoryList = React.createClass ({
 	getInitialState: function(){
 		//always returns an object
 		return{stories:[]};
 	},
 	componentDidMount: function(){
-		StoryCollection.on('add change remove destroy', ()=>{//i can put the eventS i need here
 			console.log('this.state=', this.state);
-			this.setState({stories:StoryCollection})
-		});
+			this.setState({stories:StoryCollection});
+		
 		StoryCollection.fetch();
 		console.log('If you can read this, you got to componentDidMount.');
 	},
@@ -49,10 +28,12 @@ const StoryList = React.createClass ({
 
 		});
 		return(
-			
-			)
+			<section className="page-stories">
+				<h3>Stories</h3>
+				<h1>Pick a Story</h1>
+				{storyRows}
+			</section>
+			);
 	}
 });
-//componentDidMount
-
-//render
+export default StoryList;
