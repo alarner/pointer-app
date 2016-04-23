7da1d0a57f5f45a9df92908c5f5d333fe0dd855b
Katy <ihatetoast@gmail.com>
2016-04-23 14:02:55 -0500

    ajax 75 pc done. looking for nec component

--- public/scripts/components/pages/Stories.js
+++ public/scripts/components/pages/Stories.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import StoryCollection from './../collections/';
+import Backbone from 'backbone';
+import CategoriesPreview from './sub-components/Categories-preview.js';
 
 export default React.createClass({
 	render: function() {
@@ -9,3 +12,47 @@ export default React.createClass({
 		);
 	}
 });
+
+
+///fetch the list of stories
+// const StoryCollection = Backbone.Collection.extend({
+// 	model: Story,
+// 	url: '/api/v1/story'
+// });
+
+// export default new StoryCollection();
+
+// set the initial state
+const StoryList = React.createClass ({
+	getInitialState: function(){
+		//always returns an object
+		return{stories:[]};
+	},
+	componentDidMount: function(){
+		StoryCollection.on('add change remove destroy', ()=>{//i can put the eventS i need here
+			console.log('this.state=', this.state);
+			this.setState({stories:StoryCollection})
+		});
+		StoryCollection.fetch();
+		console.log('If you can read this, you got to componentDidMount.');
+	},
+	render: function(){
+		const storyRows = this.state.stories.map((storyval, i, array)=>{
+			return(
+				<CategoriesPreview
+					key={i}
+					title={storyval.get('title')}
+					name = {storyval.get('author')}
+					category = {storyval.get('category')}
+					coverimage={storyval.get('coverimage')} />
+				);
+
+		});
+		return(
+			
+			)
+	}
+});
+//componentDidMount
+
+//render
