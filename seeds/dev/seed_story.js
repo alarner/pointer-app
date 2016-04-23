274e643ed073aaba70544b818f5449dc185860ff
anna hunter <annfmonty@gmail.com>
2016-04-23 15:41:23 -0500

    stories in the db no users

--- seeds/dev/seed_story.js
+++ seeds/dev/seed_story.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex, Promise) {
+		var date = new Date();
   return Promise.join(
+
     // Deletes ALL existing entries
-    knex('stories').del(), 
+    //knex('stories').del(), 
 
     // Inserts seed entries
-    knex('stories').insert({id: 1, title: 'Jess and her Avocado Adventure', author: 'Casanda Katydid', coverImage: 'https://s-media-cache-ak0.pinimg.com/736x/0b/ec/1e/0bec1e70af33690796667d17bdadbd3a.jpg', category: 'fable'}),
-
-    knex('stories').insert({id: 2, title: 'Will and the missing Pajamas', author: 'Buchman Daniels', coverImage: 'http://thumb1.shutterstock.com/display_pic_with_logo/925151/205278238/stock-photo-superhero-boy-child-flying-upwards-through-the-clouds-205278238.jpg', category: 'fable'}),
-    knex('stories').insert({id: 3, title: 'Sarah loves Sunshine', author: 'Anna Banana', coverImage: 'http://blog.swap-bot.com/wp-content/uploads/2012/03/kateendle.jpg', category: 'fable'})
+    knex('stories').insert({id: 1, createdAt: date, title: 'Jess and her Avocado Adventure', author: 'Casanda Katydid', coverImage: 'https://s-media-cache-ak0.pinimg.com/736x/0b/ec/1e/0bec1e70af33690796667d17bdadbd3a.jpg', category: 'fable'}),
+    knex('stories').insert({id: 2, createdAt: date, title: 'Will and the missing Pajamas', author: 'Buchman Daniels', coverImage: 'http://thumb1.shutterstock.com/display_pic_with_logo/925151/205278238/stock-photo-superhero-boy-child-flying-upwards-through-the-clouds-205278238.jpg', category: 'fable'}),
+    knex('stories').insert({id: 3, createdAt: date, title: 'Sarah loves Sunshine', author: 'Anna Banana', coverImage: 'http://blog.swap-bot.com/wp-content/uploads/2012/03/kateendle.jpg', category: 'fable'})
   );
 };
--- seeds/dev/seed_users.js
+++ seeds/dev/seed_users.js
@@ -1,12 +1,13 @@
 
 exports.seed = function(knex, Promise) {
+	var date = new Date();
   return Promise.join(
     // Deletes ALL existing entries
-    knex('users').del(), 
-
+    //knex('users').del(), 
+  
     // Inserts seed entries
-    knex('users').insert({id: 1, firstName: 'Bob', lastName: 'Shephard', email: 'bob.shephard@dogmail.com'}),
-    knex('users').insert({id: 2, firstName: 'Sarah', lastName: 'Red', email: 'sarah.red@home.com'}),
-    knex('users').insert({id: 3, firstName: 'Aaron', lastName: 'Larner', email: 'aaron.larner@theironyard.com'})
+    knex('users').insert({id: 1, createdAt: date, firstName: 'Bob', lastName: 'Shephard', password: '12345', email: 'bob.shephard@dogmail.com'}),
+    knex('users').insert({id: 2, createdAt: date, firstName: 'Sarah', lastName: 'Red', password: '12345', email: 'sarah.red@home.com'}),
+    knex('users').insert({id: 3, createdAt: date, firstName: 'Aaron', lastName: 'Larner', password: '12345', email: 'aaron.larner@theironyard.com'})
   );
 };
