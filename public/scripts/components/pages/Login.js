1a6b24d7d0ce4868e58c21288696ecfc93d87f87
Haythem Hammour <hammour@gmail.com>
2016-04-23 18:17:55 -0500

    switching branch

--- public/scripts/components/pages/Login.js
+++ public/scripts/components/pages/Login.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import $ from 'jquery';
+import {browserHistory} from 'react-router';
+import user from '../../models/user';
+
+
+
+
 
 export default React.createClass({
+		getInitialState: function() {
+		return {
+			errors: {},
+			user: user
+		};
+	},
+
 	render: function() {
 		return (
-			<section className="page-login">
-				<form className="login-box"> Login
-					<input type="text" placeholder="email"/>
-					<div className="error"> </div> 
-					<input type="password" placeholder="password"/>
-					<div className="error"> </div>
-					<button> Login </button>
+			<section className="page-login seven columns">
+				<form onSubmit={this.login} className="login-box"> Login
+					<input className="u-full-width" type="text" placeholder="email" ref='email'/>
+					<div className="error">{this.state.errors.email ? this.state.errors.email.message : null} </div> 
+					<input className="u-full-width" type="password" placeholder="password" ref='password'/>
+					<div className="error">{this.state.errors.password ? this.state.errors.password.message : null} </div>
+					<button className="button-primary" type='submit'> Login </button>
 				</form>
 			</section>
 		);
+	},
+	login: function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '/auth/login',
+		type: 'POST',
+		data: {
+			email: this.refs.email.value,
+			password: this.refs.password.value
+		},
+		headers: {
+			Accept: 'application/json'
+		},
+		success: (loggedArg) => {
+				
+		
+			this.state.user.set(loggedArg);
+			browserHistory.push('/dashboard');
+		},
+		error: (errorArg) => {
+				
+				// need to check the error messages coming from the backend to be more specific and redirects to registeration page incase of invalid account.
+				this.setState({errors: errorArg.responseJSON});
+			}
+		});
 	}
 });
+
+
+
