07b656a6f84b1b88cd238241389ee1c892636773
Dana <dana.lachman@gmail.com>
2016-04-23 15:58:39 -0500

    added logo to images folder, built out navigation component. waiting to test it

--- public/scripts/components/Navigation.js
+++ public/scripts/components/Navigation.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import {Link, browserHistory} from 'react-router';
+import $ from 'jquery';
+import user from '../models/User';
 
 export default React.createClass({
+	getInitialState: function() {
+		return {
+			user: user
+		};
+		console.log(this.state.user);
+	},
+	compondentDidMount: function() {
+		this.state.user.on('change', () => {
+			this.setState({user: user});
+		});
+	},
+
 	render: function() {
-		return (
+		if(this.state.user.get('id')) {
+			return (
 			<nav>
-				This is the navigation
+				<img src="" />
+				<a href="#" onClick={this.logout}>Logout</a>
 			</nav>
 		);
+
+		} else {
+			return (
+			<nav>
+				<img src="" />
+				<Link to="/contact">Contact</Link> 
+				<Link to="login">Login</Link> 
+			</nav>
+			);
+		}	
+	},
+	logout: function(e) {
+		e.preventDefault();
+		console.log('clicky click click');
+		console.log(this.state.user);
+		this.state.user.clear();
+		$.ajax({
+			type: 'POST',
+			url: 'auth/logout'
+		});
+		browserHistory.push('/');
 	}
 });
