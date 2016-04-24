3e5d2631fa63551c1fa31e63ae395d9685c10a6e
Will Cook <wellsee83@gmail.com>
2016-04-23 21:05:51 -0500

    Digging through data

--- public/scripts/components/pages/Stories.js
+++ public/scripts/components/pages/Stories.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import Stories from './../../collections/StoryCollection';
+import CategoriesPreview from './../sub-components/Categories-preview.js';
 
-export default React.createClass({
-	render: function() {
-		return (
+
+const StoryList = React.createClass ({
+	getInitialState: function(){
+		//always returns an object
+		return{Stories:[]};
+		console.log(this.state);
+	},
+	componentDidMount: function(){
+			// console.log('this.state=', this.state.stories);
+		Stories.on('update', () =>{
+			this.setState({Stories: Stories})
+		});
+		Stories.fetch();
+		console.log(Stories);
+		console.log('If you can read this, you got to componentDidMount.');
+	},
+	render: function(){
+		console.log(this.state);
+		console.log(Stories.models);
+		const storyRows = this.state.Stories.map((storyval, i, array)=>{
+			return(
+				<CategoriesPreview
+					key={i}
+					title={storyval.get('title')}
+					name = {storyval.get('author')}
+					category = {storyval.get('category')}
+					coverimage={storyval.get('coverimage')} />
+				);
+
+		});
+		return(
 			<section className="page-stories">
-				<h1>Stories</h1>
+				<h3>Stories</h3>
+				<h1>Pick a Story</h1>
+				{storyRows}
 			</section>
-		);
+			);
+		console.log({storyRows});
 	}
 });
+export default StoryList;
