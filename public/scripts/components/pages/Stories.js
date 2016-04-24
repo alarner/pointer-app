11b5b0c48feb077f6811af3e2b88309e0371a433
Haythem Hammour <hammour@gmail.com>
2016-04-24 14:46:39 -0500

    added titles to input field and required them before submitting

--- public/scripts/components/pages/Stories.js
+++ public/scripts/components/pages/Stories.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import StoryCollection from './../collections/';
-import CategoriesPreview from './sub-components/Categories-preview.js';
 
-
-const StoryList = React.createClass ({
-	getInitialState: function(){
-		//always returns an object
-		return{stories:[]};
-	},
-	componentDidMount: function(){
-			console.log('this.state=', this.state);
-			this.setState({stories:StoryCollection});
-		
-		StoryCollection.fetch();
-		console.log('If you can read this, you got to componentDidMount.');
-	},
-	render: function(){
-		const storyRows = this.state.stories.map((storyval, i, array)=>{
-			return(
-				<CategoriesPreview
-					key={i}
-					title={storyval.get('title')}
-					name = {storyval.get('author')}
-					category = {storyval.get('category')}
-					coverimage={storyval.get('coverimage')} />
-				);
-
-		});
-		return(
+export default React.createClass({
+	render: function() {
+		return (
 			<section className="page-stories">
-				<h3>Stories</h3>
-				<h1>Pick a Story</h1>
-				{storyRows}
+				<h1>Stories</h1>
 			</section>
-			);
+		);
 	}
 });
-export default StoryList;
