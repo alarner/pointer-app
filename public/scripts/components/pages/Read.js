fef4513c1cf7b73f35698741c86810f3700f6626
Nate Hawthorne <natejhawthorne@gmail.com>
2016-04-25 12:09:26 -0500

    Added url verification

--- public/scripts/components/pages/Read.js
+++ public/scripts/components/pages/Read.js
@@ -1,21 +1,38 @@
 import React from 'react';
-// import Stories from './../../collections/StoryCollection.js';
+import StoryModel from './../../models/StoryModel.js';
 
 export default React.createClass({
-	// getInitialState: function() {
-	// 	console.log('getInitialState');
-	// 	console.log(this.state);
-	// 	return {
-	// 		Stories: Stories
-	// 	};
-	// },
-	// componentDidMount: function() {
-	// 	console.log('componentDidMount');
-	// 	console.log(this.state);
-	// 	this.state.Stories.fetch();
+	getInitialState: function() {
+		console.log('getInitialState');
+		console.log(this.state);
+		
+		return {
+			story: new StoryModel({id: this.props.params.storyId}),
+			error: ''
+		};
+	},
+	componentDidMount: function() {
+		console.log('componentDidMount');
+		console.log(this.state);
+		console.log(this.state.story);
+		console.log('change');
+		this.state.story.on('change', (storyData)=> {
+			console.log('story changed');
+			this.setState({story: this.state.story});
+		});
+		// console.log('error');
+		// this.state.story.on('error', (error) => {
+		// 	console.log(error);
+		// 	this.setState({error: error});
+		// });
+		console.log('fetch');
+		this.state.story.fetch({error: (model, error) => {
+			console.log('fetch error', error);
+			this.setState({error: error.responseJSON.message});
+		}});
+		console.log(this.state);
+		console.log(this.state.story);
 		// this.state.Stories.get();
-		// console.log(this.state);
-		// console.log(this.state.Stories);
 		// console.log(this.state.Stories.get('models'));
 		// const stories = this.state.Stories.map((val, i, arr) => {
 		// 	return val;
@@ -23,14 +40,22 @@ export default React.createClass({
 		// for (var i = 0; i < this.state.Stories.length; i++) {
 		// 	this.state.Stories[i]
 		// }
-	// },
+	},
 	render: function() {
-		// console.log('render');
-		// console.log(this.state);
-		return (
-			<section className="page-read">
-				<h1>Read</h1>
-			</section>
-		);
+		console.log('render');
+		console.log(this.state);
+		if(this.state.error === '') {
+			return (
+				<section className="page-read">
+					<h1>Read</h1>
+				</section>
+			);
+		} else {
+			return (
+				<section className="page-read">
+					<h1>{this.state.error}</h1>
+				</section>
+			);
+		}
 	}
 });
