59ea7937ddc1fc28c3754fd867aef0e756b2b859
Dan Buda <danbuda@gmail.com>
2016-04-25 16:08:09 -0500

    routes fixed

--- public/scripts/components/sub-components/PicTitleThumb.js
+++ public/scripts/components/sub-components/PicTitleThumb.js
@@ -5,7 +5,7 @@ export default React.createClass({
 	render: function() {
 		return (
 		<div className="story-thumb-box" >
-			<Link to="/stories/:storyId/details">
+			<Link to={`/stories/${this.props.id}/details`}>
 				<img className="story-thumb-pic" src={this.props.coverImage}/>
 			</Link>
 			<Link to="/stories/:storyId/details" className="story-thumb-title"><h2>{this.props.title}</h2></Link>
