229d7be6f444203f74fa296bd5898dcc78df4cf0
Jen Udan <you@example.com>
2016-04-25 15:03:04 -0500

    moved dropdown subcomponent code into details page

--- public/scripts/components/pages/Details.js
+++ public/scripts/components/pages/Details.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import StudentDropdown from '../sub-components/StudentDropdown.js'
+
+import students from './../../collections/StudentsCollection.js'; 
 
 export default React.createClass({
+	getInitialState: function() {
+		return {
+			students: students
+		};
+	},
+	componentDidMount: function() {
+		students.on('update', () => {
+			this.setState({students: students});
+		});
+		students.fetch();
+	},
 	render: function() {
+		const allStudents = this.state.students.map((student, index, array) => {
+		return (
+			<option value="Student" key={index}>{student.get('firstName')} {student.get('lastName')}</option> 
+			)
+		});
 		return (
 			<section className="page-details">
 				<h1>Details</h1>
-				<StudentDropdown />
+				<div className="student-dropdown-component">
+					<p className="student-prompt">Which student is participating?</p>
+					<div align="center">
+						<select name="mydropdown">
+							<option value="Pick">Pick a student</option>
+							{allStudents}
+						</select>
+					</div>
+				</div>
 			</section>
 		);
 	}
