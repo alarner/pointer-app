1914a7ec7efb542478a4619ad317ed53bffaafb9
Katy <ihatetoast@gmail.com>
2016-04-25 10:50:12 -0500

    fetch stories done and tested

--- public/scripts/components/pages/Stories.js
+++ public/scripts/components/pages/Stories.js
@@ -1,37 +1,40 @@
 import React from 'react';
-import StoryCollection from './../collections/';
-import CategoriesPreview from './sub-components/Categories-preview.js';
+import StoryCollection from './../../collections/StoryCollection';
+
 
 
 const StoryList = React.createClass ({
 	getInitialState: function(){
-		//always returns an object
-		return{stories:[]};
+		return{
+			stories: StoryCollection
+		};
 	},
 	componentDidMount: function(){
-			console.log('this.state=', this.state);
-			this.setState({stories:StoryCollection});
+		console.log('this.state=', this.state);
+		StoryCollection.on('update', () =>{
+            this.setState({stories: StoryCollection});
+        });
 		
 		StoryCollection.fetch();
 		console.log('If you can read this, you got to componentDidMount.');
 	},
 	render: function(){
-		const storyRows = this.state.stories.map((storyval, i, array)=>{
-			return(
-				<CategoriesPreview
-					key={i}
-					title={storyval.get('title')}
-					name = {storyval.get('author')}
-					category = {storyval.get('category')}
-					coverimage={storyval.get('coverimage')} />
-				);
+		// const storyRows = this.state.stories.map((storyval, i, array)=>{
+		// 	return(
+		// 		<CategoriesPreview
+		// 			key={i}
+		// 			title={storyval.get('title')}
+		// 			name = {storyval.get('author')}
+		// 			category = {storyval.get('category')}
+		// 			coverimage={storyval.get('coverimage')} />
+		// 		);
 
-		});
+		// });
 		return(
 			<section className="page-stories">
 				<h3>Stories</h3>
 				<h1>Pick a Story</h1>
-				{storyRows}
+
 			</section>
 			);
 	}
