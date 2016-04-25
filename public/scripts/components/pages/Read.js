15bf080ee047387cbfa428016c39c4fded286d4d
Nate Hawthorne <natejhawthorne@gmail.com>
2016-04-25 10:12:15 -0500

    Commented out future work so we can have a read page

--- public/scripts/components/pages/Read.js
+++ public/scripts/components/pages/Read.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import Stories from './../../collections/StoryCollection.js';
+// import Stories from './../../collections/StoryCollection.js';
 
 export default React.createClass({
-	getInitialState: function() {
-		console.log('getInitialState');
-		console.log(this.state);
-		return {
-			Stories: Stories
-		};
-	},
-	componentDidMount: function() {
-		// console.log('componentDidMount');
-		// console.log(this.state);
-		this.state.Stories.fetch();
+	// getInitialState: function() {
+	// 	console.log('getInitialState');
+	// 	console.log(this.state);
+	// 	return {
+	// 		Stories: Stories
+	// 	};
+	// },
+	// componentDidMount: function() {
+	// 	console.log('componentDidMount');
+	// 	console.log(this.state);
+	// 	this.state.Stories.fetch();
 		// this.state.Stories.get();
-		console.log(this.state);
-		console.log(this.state.Stories);
-		console.log(this.state.Stories.get('models'));
+		// console.log(this.state);
+		// console.log(this.state.Stories);
+		// console.log(this.state.Stories.get('models'));
 		// const stories = this.state.Stories.map((val, i, arr) => {
 		// 	return val;
 		// });
 		// for (var i = 0; i < this.state.Stories.length; i++) {
 		// 	this.state.Stories[i]
 		// }
-
-	},
+	// },
 	render: function() {
-		console.log('render');
-		console.log(this.state);
+		// console.log('render');
+		// console.log(this.state);
 		return (
 			<section className="page-read">
 				<h1>Read</h1>
