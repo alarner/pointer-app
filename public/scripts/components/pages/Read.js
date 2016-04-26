074468ed27025a80e1bd8ec7af556dc897251f62
Will Cook <wellsee83@gmail.com>
2016-04-26 15:41:55 -0500

    Styles in progress

--- public/scripts/components/pages/Read.js
+++ public/scripts/components/pages/Read.js
@@ -38,13 +38,13 @@ export default React.createClass({
 			return (
 				<section className="page-read">
 					<h1>Naked Creature Finds a Stream</h1>
-					<img className="eleven columns" src='https://trello-attachments.s3.amazonaws.com/571f7f7e00a12eb39662ee19/543x372/b10e6f962dbf447608f17601354f7927/upload_4_26_2016_at_9_47_25_AM.png'/>
-					<p className="eleven columns">Whilst happening upon a stream, our creature friend exclaims, "Well would you look at that - a stream!!"</p>
-					<div className="eight columns directionals-container">
-						<button className="directionals">Back</button>
-						<button className="directionals">Forward</button>
+					<img className="page-pic" src='https://trello-attachments.s3.amazonaws.com/571f7f7e00a12eb39662ee19/543x372/b10e6f962dbf447608f17601354f7927/upload_4_26_2016_at_9_47_25_AM.png'/>
+					<p className="page-text">Whilst happening upon a stream, our creature friend exclaims, "Well would you look at that - a stream!!"</p>
+					<div className="directionals-container">
+						<button className="directionals">Previous</button>
+						<h1 className="page-num">Pg. 1</h1>
+						<button className="directionals">Next</button>
 					</div>
-					<h1>Pg. 1</h1>
 				</section>
 			);
 
