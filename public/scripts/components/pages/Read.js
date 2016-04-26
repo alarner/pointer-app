1d148a214d9eb307a4beac0344b6e01209c5f7eb
Will Cook <wellsee83@gmail.com>
2016-04-26 13:49:11 -0500

    Beginning structures

--- public/scripts/components/pages/Read.js
+++ public/scripts/components/pages/Read.js
@@ -37,7 +37,15 @@ export default React.createClass({
 		else {
 			return (
 				<section className="page-read">
-					<h1>Read</h1>
+					<h1>Naked Creature Finds a Stream</h1>
+					<img className="eleven columns" src='https://trello-attachments.s3.amazonaws.com/571f7f7e00a12eb39662ee19/543x372/b10e6f962dbf447608f17601354f7927/upload_4_26_2016_at_9_47_25_AM.png'/>
+					<p className="eleven columns">Whilst happening upon a stream, our creature friend exclaims, "Well would you look at that - a stream!!"</p>
+					<div className="eight columns directionals">
+						<button>Back</button>
+						<button>Play</button>
+						<button>Forward</button>
+					</div>
+					<h1>Pg. 1</h1>
 				</section>
 			);
 
