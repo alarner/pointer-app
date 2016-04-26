4cae4584dd99711cdeafedc530da98622e95af67
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-26 14:59:08 -0500

    changed class names on input fields

--- public/scripts/components/pages/Admin.js
+++ public/scripts/components/pages/Admin.js
@@ -14,7 +14,7 @@ export default React.createClass({
 	createTeacher: function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: '/auth/login',
+		url: '/auth/add-user',
 		type: 'POST',
 		data: {
 			
@@ -48,7 +48,7 @@ export default React.createClass({
 	},
 
 	generatePassword: function() {
- 	   this.refs.password.value = randomPW();
+ 	   this.refs.password.value = this.randomPW();
 	},
 
 
@@ -57,15 +57,15 @@ export default React.createClass({
 		return (
 			<section> 
 				<h1>Add a new Teacher</h1>
-				<form className="create-teacher" on Submit={this.createTeacher}>
+				<form className="create-teacher" onSubmit={this.createTeacher}>
 					<h2>Name</h2>
-					<input className="u-full-width" type="text" placeholder="First" ref="first" title="First name is required and cannot left blank" required="required"/>
-					<input className="u-full-width" type="text" placeholder="Last" ref="last" title="Last name is required and cannot left blank" required="required"/>/>
+					<input className="u-half-width" type="text" placeholder="First" ref="first" title="First name is required and cannot left blank" required="required"/>
+					<input className="u-half-width" type="text" placeholder="Last" ref="last" title="Last name is required and cannot left blank" required="required"/>
 					<h2>Email</h2>
-					<input className="u-full-width" type="text" ref="email" title="Should be a valid email address" required="required"/>
+					<input className="u-half-width" type="text" ref="email" title="Should be a valid email address" required="required"/>
 					<h2>Password</h2>
-					<input className="u-full-width" type="password" ref='password' title="Password is required and cannot left blank" required="required"/>
-					<button className="button-generate" type="button" on Click={this.generatePassword}>Generate Password</button>
+					<input className="u-half-width" type="text" ref='password' title="Password is required and cannot left blank" required="required"/>
+					<button className="button-generate" type="button" onClick={this.generatePassword}>Generate Password</button>
 					<button className="button-primary" type='submit'>Create Teacher</button>
 				</form>
 			</section>  
