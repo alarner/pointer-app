3e341bb2f8b82f4e4ec700f91ac86a8c7a97e77a
Dana <dana.lachman@gmail.com>
2016-04-28 15:43:10 -0500

    fixed category dropdown merge conflict and added styles

--- public/scripts/components/Navigation.js
+++ public/scripts/components/Navigation.js
@@ -22,14 +22,12 @@ export default React.createClass({
 		});
 		Stories.on('update', () =>{
 			this.setState({
-				Stories: Stories,
-				//categories: categories
+				Stories: Stories
 			});
 		});	
 	},
 
 	render: function() {
-<<<<<<< HEAD
 		  const categories = Stories.pluck('category'); 
 		 	let catList = [];
 		 	categories.forEach(function(cat, i){
@@ -40,24 +38,21 @@ export default React.createClass({
 		 	 const allCategories = catList.map((val, i) => {
 		 	 	return <option key = {i}>{val}</option>;
 		 	 });
-		
-		if(this.state.user.get('id')) {
-			return (<nav>
-			<img src="/images/pointer_logo.png" />
-			<div className="nav-cat-dropdown">	
-				<select value = "Select a category" name="mydropdown" onChange={this.selectCat}>
+			
+			let categoriesDropdown = (<div className="nav-cat-dropdown">	
+				<select ref = "category" name="mydropdown" onChange={this.selectCat}>
 					<option value="Pick">Go to Category</option>
 							{allCategories}
 				</select>
-			</div>
-=======
-		if (this.state.user.attributes.userType === 'Admin') {
+			</div>);
+			
+		if (this.state.user.get('userType') === 'Admin') {
 			return (<nav>
 			<Link to="/stories"><img src="/images/pointer_logo.png" /></Link>
->>>>>>> 3c4299e876960e44e7ebdc6c4961086d7debb0e6
 			<a href="#" className="nav-links" onClick={this.logout}>Logout</a>
 			<Link className="nav-links" to="/stories">Stories</Link>
 			<Link className="nav-links" to="/Admin">Add User</Link>
+			{categoriesDropdown}
 		</nav>);
 		}
 		else if(this.state.user.get('id')) {
@@ -75,8 +70,9 @@ export default React.createClass({
 		}
 	},
 	 selectCat: function(e) {
-     	this.setState({currentCategory: e.target.value});
+     	browserHistory.push('/stories/' + this.refs.category.value);
      },
+
 	logout: function(e) {
 		e.preventDefault();
 		this.state.user.clear();
